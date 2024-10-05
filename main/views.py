07fd97e69f01210f31e6109edb2ef1205b1fe101
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-10-05 14:59:00 +0700

    update Tugas 6

--- main/views.py
+++ main/views.py
@@ -12,13 +12,10 @@ from main.models import Product
 
 @login_required(login_url='/login')
 def show_main(request) :
-    products = Product.objects.filter(user=request.user)
-
     context = {
         'app_name' : 'marquette',
         'name': request.user.username,
         'class' : 'PBP D',
-        'products' : products,
         'last_login': request.COOKIES['last_login'],
     }
     
@@ -37,20 +34,22 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+   
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -87,7 +86,7 @@ def delete_product(request, id) :
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request) :
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_xml_by_id(request, id) :
@@ -95,7 +94,7 @@ def show_xml_by_id(request, id) :
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request) :
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_json_by_id(request, id) :
